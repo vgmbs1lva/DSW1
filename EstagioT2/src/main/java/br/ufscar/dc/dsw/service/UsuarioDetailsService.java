a8b85170c870330bd6d7c93461f92607503a68ed
vgmbs1lva <135669181+vgmbs1lva@users.noreply.github.com>
2024-08-19 17:34:59 -0300

    Update UsuarioDetailsService.java

--- EstagioT2/src/main/java/br/ufscar/dc/dsw/service/UsuarioDetailsService.java
+++ EstagioT2/src/main/java/br/ufscar/dc/dsw/service/UsuarioDetailsService.java
@@ -1,29 +1,34 @@
 package br.ufscar.dc.dsw.service;
 
-import br.ufscar.dc.dsw.dao.IUsuarioDAO;
-import br.ufscar.dc.dsw.domain.Usuario;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import br.ufscar.dc.dsw.domain.Usuario;
+import br.ufscar.dc.dsw.repository.IUsuarioRepository;
+
+import java.util.Collections;
+
 @Service
 public class UsuarioDetailsService implements UserDetailsService {
 
     @Autowired
-    private IUsuarioDAO dao;
+    private IUsuarioRepository usuarioRepository;
 
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
-        Usuario usuario = dao.findByEmail(email)
+        Usuario usuario = usuarioRepository.findByEmail(email)
                 .orElseThrow(() -> new UsernameNotFoundException("Usuário não encontrado"));
-
-        return User.builder()
-                .username(usuario.getEmail())
-                .password(usuario.getSenha())
-                .roles(usuario.getRole())
-                .build();
+        
+        GrantedAuthority authority = new SimpleGrantedAuthority(usuario.getRole());
+        return new org.springframework.security.core.userdetails.User(
+                usuario.getEmail(),
+                usuario.getSenha(),
+                Collections.singletonList(authority)
+        );
     }
 }
