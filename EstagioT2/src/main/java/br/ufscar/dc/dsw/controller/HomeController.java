7783b0bd465f1d2806ec55fb882657c73f131a51
vgmbs1lva <135669181+vgmbs1lva@users.noreply.github.com>
2024-08-23 11:31:40 -0300

    Update HomeController.java

--- EstagioT2/src/main/java/br/ufscar/dc/dsw/controller/HomeController.java
+++ EstagioT2/src/main/java/br/ufscar/dc/dsw/controller/HomeController.java
@@ -1,13 +1,38 @@
 package br.ufscar.dc.dsw.controller;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Set;
+
+import jakarta.servlet.http.HttpServletResponse;
 
 @Controller
+@RequestMapping("/home")
 public class HomeController {
 
-    @GetMapping("/home")
-    public String home() {
-        return "home";  // Retorna o template "home.html" localizado em src/main/resources/templates/
+    @GetMapping
+    public String homeRedirect(Authentication authentication, HttpServletResponse response) {
+        Set<String> roles = AuthorityUtils.authorityListToSet(authentication.getAuthorities());
+
+        try {
+            if (roles.contains("ROLE_ADMIN")) {
+                response.sendRedirect("/admin/home");
+            } else if (roles.contains("ROLE_EMPRESA")) {
+                response.sendRedirect("/empresa/listar");
+            } else if (roles.contains("ROLE_PROFISSIONAL")) {
+                response.sendRedirect("/profissional/home");
+            } else {
+                response.sendRedirect("/default");  // Página padrão caso nenhuma role seja encontrada
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            return "error";
+        }
+
+        return null; // Retornando null para garantir que não haja mais processamento.
     }
 }
